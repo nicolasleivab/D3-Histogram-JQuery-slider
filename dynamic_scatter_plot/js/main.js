ff5147edeec780979655892089105cdf5f776e3b
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-03-18 19:10:52 +0100

    legend

--- dynamic_scatter_plot/js/main.js
+++ dynamic_scatter_plot/js/main.js
@@ -22,14 +22,14 @@ var time = 0;
 var x = d3.scaleLog()
     .base(10)
     .range([0, width])
-    .domain([300, 150000]);
+    .domain([142, 150000]);
 var y = d3.scaleLinear()
     .range([height, 0])
     .domain([0, 90]);
 var area = d3.scaleLinear()
     .range([25*Math.PI, 1500*Math.PI])
     .domain([2000, 1400000000]);
-var continentColor = d3.scaleOrdinal(d3.schemeSet3);
+var continentColor = d3.scaleOrdinal(d3.schemeAccent);
 
 // Labels
 var xLabel = g.append("text")
@@ -48,7 +48,7 @@ var yLabel = g.append("text")
 var timeLabel = g.append("text")
     .attr("y", height -10)
     .attr("x", width - 40)
-    .attr("font-size", "45px")
+    .attr("font-size", "40px")
     .attr("opacity", "0.7")
     .attr("text-anchor", "middle")
     .text("1800");
@@ -67,12 +67,34 @@ var yAxisCall = d3.axisLeft(y)
     .tickFormat(function(d){ return +d; });
 g.append("g")
     .attr("class", "y axis")
-    .call(yAxisCall);
+    .call(yAxisCall); 
 
+var continents = ["europe", "asia", "americas", "africa"]; //group continents
+
+var legend = g.append("g")
+    .attr("transform", "translate(" + (width - 10) + 
+        "," + (height - 125) + ")");
+
+continents.forEach(function(continent, i){ //function to append each element from the group
+    var legendRow = legend.append("g")
+        .attr("transform", "translate(0, " + (i * 20) + ")");
+
+    legendRow.append("rect")  // rects for the legend
+        .attr("width", 10)
+        .attr("height", 10)
+        .attr("fill", continentColor(continent));
+
+    legendRow.append("text")   //actual text for the legend
+        .attr("x", -10)
+        .attr("y", 10)
+        .attr("text-anchor", "end")
+        .style("text-transform", "capitalize")
+        .text(continent);
+});
 d3.json("data/data.json").then(function(data){
     console.log(data);
 
-    // Clean data Filter on the array to exclude null values in the country-year data points from the dataset.
+    // Clean data
     const formattedData = data.map(function(year){
         return year["countries"].filter(function(country){
             var dataExists = (country.income && country.life_exp);
@@ -84,12 +106,12 @@ d3.json("data/data.json").then(function(data){
         })
     });
 
-    // Run the code every 0.1 second
+    // Run the code every 0.11 second
     d3.interval(function(){
         // At the end of our data, loop back
         time = (time < 214) ? time+1 : 0
         update(formattedData[time]);            
-    }, 100);
+    }, 110);
 
     // First run of the visualization
     update(formattedData[0]);
@@ -97,9 +119,9 @@ d3.json("data/data.json").then(function(data){
 })
 
 function update(data) {
-    // defining transition time for visualization
+    // Standard transition time for the visualization
     var t = d3.transition()
-        .duration(111);
+        .duration(100);
 
     // JOIN new data with old elements.
     var circles = g.selectAll("circle").data(data, function(d){
@@ -117,7 +139,7 @@ function update(data) {
         .attr("class", "enter")
         .attr("fill", function(d) { return continentColor(d.continent); })
         .merge(circles)
-        .transition(t)      
+        .transition(t)
             .attr("cy", function(d){ return y(d.life_exp); })
             .attr("cx", function(d){ return x(d.income) })
             .attr("r", function(d){ return Math.sqrt(area(d.population) / Math.PI) });
