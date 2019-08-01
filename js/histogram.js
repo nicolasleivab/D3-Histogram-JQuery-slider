bfabdd8b701986355c522a5ba53d4a0885dcf133
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-08-01 21:53:05 +0200

    dimensions and svg append

--- js/histogram.js
+++ js/histogram.js
@@ -1,2 +1,12 @@
 /* D3 Histogram */
 
+//Set dimensions and append svg to div #histogram
+
+var svg = d3.select("#histogram"),
+    margin = {top: 20, right: 120, bottom: 100, left: 100},
+    width = 1200 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom,
+    g = svg.append("svg").attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
