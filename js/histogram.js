04af8662d41aea9f5fd3fc669cc25665cf02b505
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-08-01 22:38:21 +0200

    call axis

--- js/histogram.js
+++ js/histogram.js
@@ -1,7 +1,6 @@
 /* D3 Histogram */
 
-//Set dimensions and append svg to div #histogram
-
+// Set dimensions and append svg to div #histogram
 var svg = d3.select("#histogram"),
     margin = {top: 20, right: 120, bottom: 100, left: 100},
     width = 1200 - margin.left - margin.right,
@@ -11,13 +10,12 @@ var svg = d3.select("#histogram"),
         .append("g")
         .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
-// Load data from csv
 
-d3.tsv("/data/asd.tsv")
+// Load data from csv
+d3.tsv("/data/data.tsv")
     .then(function(data){
 
 // Format Data
-
 var parseDate = d3.timeParse("%m/%d/%Y");
 
 data.forEach(function(d) {
@@ -29,7 +27,28 @@ data.forEach(function(d) {
 
 console.log(data);
 
+// X axis and call func
+var x = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return d['Product A'] + d['Product A']/10; })]) // Get max of the selected filter as domain
+      .range([0, width]);
+g.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x)); //call x axis
+
 });
 
+// Setting Histogram parameters
+var histogram = d3.histogram()
+    .value(function(d) { return d['Product A']; })   //Value of the vector
+    .domain(x.domain())  //load x domain
+    .thresholds(x.ticks(10)); //Set number of bins
 
+var bins = histogram(data); //Apply d3.histogram function with array data as input and creat a binding 'bins'
+
+// Y axis and call func
+var y = d3.scaleLinear()
+    .range([height, 0]);
+    y.domain([0, d3.max(bins, function(d) { return d.length; })]);   //return length of selected value in hist func
+g.append("g")
+    .call(d3.axisLeft(y)); //call y axis
 
